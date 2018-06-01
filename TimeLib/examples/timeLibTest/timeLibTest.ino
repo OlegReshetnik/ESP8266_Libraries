a27c6515c1c83b0e4286f8275258246dfd18d690
Oleg <olegreshetnik@gmail.com>
2018-06-01 11:53:15 +0300

    Update timeLibTest.ino
    Update example

--- TimeLib/examples/timeLibTest/timeLibTest.ino
+++ TimeLib/examples/timeLibTest/timeLibTest.ino
@@ -1,7 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <TimeLib.h>
 
-TimeLib tt;
+TimeLib tt; // Например: TimeLib tt(2); - Калининград.
+            //Зона по умолчанию - 3 (Москва)
 
 void InitWiFi()
 {
@@ -19,6 +20,21 @@ void setup()
 
 void loop()
 {
+	/* Получить данные:
+	struct tm {
+	  int	tm_sec;
+	  int	tm_min;
+	  int	tm_hour;
+	  int	tm_mday;
+	  int	tm_mon;
+	  int	tm_year;
+	  int	tm_wday;
+	  int	tm_yday;
+	  int	tm_isdst; };
+	*/
+	struct tm c_tine = cTime();
+	Serial.printf( "Время с точками: %02d.%02d.%02d\n", c_tine->tm_hour, c_tine->tm_min, c_tine->tm_sec );
+	
 	Serial.print( tt.tmDate() );
 	Serial.print( " " );
 	Serial.println( tt.tmTime() );
