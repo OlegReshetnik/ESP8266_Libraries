6ddb8ca6da2265c3c61f254f51f4a7ab9441be37
Oleg <olegreshetnik@gmail.com>
2018-06-03 16:16:26 +0300

    Update WeatherLibTest.ino

--- WeatherLib/examples/WeatherLibTest/WeatherLibTest.ino
+++ WeatherLib/examples/WeatherLibTest/WeatherLibTest.ino
@@ -1,13 +1,22 @@
+#include <ESP8266WiFi.h>
 #include <WeatherLib.h>
 
 WeatherLib wl("Krasnodar"); // "moscow,RU" если в разных странах есть одинаковые названия
 
+void InitWiFi()
+{
+	const char * WiFi_Name = "********";
+	const char * WiFi_Pass = "********";
+	WiFi.begin( WiFi_Name, WiFi_Pass );
+	while(WiFi.waitForConnectResult() != WL_CONNECTED) WiFi.begin( WiFi_Name, WiFi_Pass );
+}
+
 void setup() {
 	Serial.begin( 115200 );
+	InitWiFi();
 }
 
 void loop() {
 	Serial.println( wl.getWeatherString() );
-
 	delay(5000);
 }
