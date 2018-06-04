ac4b37cbd59b77061827c4085702a4b2dade21e0
Oleg <olegreshetnik@gmail.com>
2018-06-04 12:09:19 +0300

    Add files via upload

--- WeatherLib/examples/WeatherLibTest/WeatherLibTest.ino
+++ WeatherLib/examples/WeatherLibTest/WeatherLibTest.ino
@@ -1,12 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <WeatherLib.h>
 
-WeatherLib wl("Krasnodar"); // "moscow,RU" если в разных странах есть одинаковые названия
+WeatherLib wl("City","api_key"); // "City,RU" если в разных странах есть одинаковые названия
+// Получить API key здесь: https://openweathermap.org/appid
 
 void InitWiFi()
 {
-	const char * WiFi_Name = "********";
-	const char * WiFi_Pass = "********";
+	const char * WiFi_Name = "********"; // SSID вашей сети
+	const char * WiFi_Pass = "********"; // Пароль вашей сети
 	WiFi.begin( WiFi_Name, WiFi_Pass );
 	while(WiFi.waitForConnectResult() != WL_CONNECTED) WiFi.begin( WiFi_Name, WiFi_Pass );
 }
@@ -18,5 +19,6 @@ void setup() {
 
 void loop() {
 	Serial.println( wl.getWeatherString() );
+
 	delay(5000);
 }
