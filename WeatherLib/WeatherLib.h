4e4d284957c87f53e95c58f5985bf0e676351c13
Oleg <olegreshetnik@gmail.com>
2018-06-04 12:04:46 +0300

    Update WeatherLib.h

--- WeatherLib/WeatherLib.h
+++ WeatherLib/WeatherLib.h
@@ -6,16 +6,16 @@
 
 #define WEATHER_UPDATE_INTERVAL	(20*60*1000) // 20 minutes
 
-#define W_W		0 // западный
+#define W_W	0 // западный
 #define W_WS	1 // юг-западный
-#define W_S		2 // южный
+#define W_S	2 // южный
 #define W_SO	3 // юго-восточный
-#define W_O		4 // восточный
+#define W_O	4 // восточный
 #define W_NO	5 // северо-восточный
-#define W_N		6 // северный
+#define W_N	6 // северный
 #define W_NW	7 // северо-западный
 
-#define DESK_LEN			31
+#define DESK_LEN		31
 #define WEATHER_READ_BUFF	300
 #define MAX_WEATHER_STRING	64
 
@@ -30,7 +30,7 @@ struct weatherSt {
 
 class WeatherLib {
 	public:
-		WeatherLib( const char * city );
+		WeatherLib( const char * city, const char * apiKey );
 		~WeatherLib();
 		weatherSt * getWeather();
 		char * getWeatherString();
@@ -38,13 +38,14 @@ class WeatherLib {
 	private:
 		void _updateWeather();
 		void _printWeather( char * str, weatherSt * _wz );
-		void _getJson( WiFiClient * pClnt, char * buff, char * city );
+		bool _getJson( WiFiClient * pClnt, char * buff, char * city, char * apiKey );
 		void _parseJson( char * buff, weatherSt * _wz );
 
 		WiFiClient * _pClient;
 		weatherSt * _pWeather;
 		char * _pWeatherString;
 		char * _pCity;
+		char * _pApiKey;
 		uint32_t _oldRead;
 };
 
