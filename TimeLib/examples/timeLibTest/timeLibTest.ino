013222b283330f0ac43214ab412715b1d49ac8c0
Oleg <olegreshetnik@gmail.com>
2022-10-21 01:46:51 +0300

    Update timeLibTest.ino

--- TimeLib/examples/timeLibTest/timeLibTest.ino
+++ TimeLib/examples/timeLibTest/timeLibTest.ino
@@ -1,8 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <TimeLib.h>
 
-TimeLib tl; // Например: TimeLib tt(2); - Калининград.
-            // Зона по умолчанию - 3 (Москва)
+TimeLib tt;
 
 void InitWiFi()
 {
@@ -20,24 +19,9 @@ void setup()
 
 void loop()
 {
-	/* Какие поля есть у структуры:
-	struct tm {
-	  int	tm_sec;
-	  int	tm_min;
-	  int	tm_hour;
-	  int	tm_mday;
-	  int	tm_mon;
-	  int	tm_year;
-	  int	tm_wday;
-	  int	tm_yday;
-	  int	tm_isdst; };
-	*/
-	struct tm * c_time = tl.cTime();
-	Serial.printf( "Время с точками: %02d.%02d.%02d\n", c_time->tm_hour, c_time->tm_min, c_time->tm_sec );
-	
-	Serial.print( tl.tmDate() );
+	Serial.print( tt.tmDate() );
 	Serial.print( " " );
-	Serial.println( tl.tmTime() );
+	Serial.println( tt.tmTime() );
 
 	delay(2000);
 }
